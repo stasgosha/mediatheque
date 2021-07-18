274079deb71d92da6f60be3aeff81ec43a096380
Vyacheslav <59470677+4zik88@users.noreply.github.com>
2021-07-18 17:36:08 +0300

    page magazine

--- partials/loop-article.php
+++ partials/loop-article.php
@@ -1,24 +1,29 @@
 
 <div <?php post_class( 'entry-article' ); ?> id="article-<?php echo $post->ID; ?>">
-		<a href="<?php the_permalink(); ?>">
-			<div class="post-thumbnail">
-				<?php H::the_post_thumbnail( 'medium' ); ?>
-			</div>
-		</a>
-		<footer class="entry-footer" data-mh="entry-footer">
-			<a href="<?php the_permalink(); ?>">
-				<h4 class="entry-title">
-					<?php the_title(); ?>
-				</h4>
-			</a>
-			<p class="entry-excerpt">
-				<?php H::the_excerpt( 15 ); ?>
-			</p>
-			<a href="<?php the_permalink(); ?>" class="entry-link icon-link">
-				<span class="icon meditech-chevron-left purpleArrowIcon"></span>
-				<span class="text">
+    <a href="<?php the_permalink(); ?>">
+        <div class="post-thumbnail">
+            <?php H::the_post_thumbnail( 'medium' ); ?>
+        </div>
+    </a>
+    <footer class="entry-footer" data-mh="entry-footer">
+        <a href="<?php the_permalink(); ?>">
+            <h4 class="entry-title">
+                <?php the_title(); ?>
+            </h4>
+        </a>
+
+        <div class="entry-meta">
+            30.01.21 /  אסף גמזו
+        </div>
+
+        <p class="entry-excerpt">
+            <?php H::the_excerpt( 15 ); ?>
+        </p>
+        <a href="<?php the_permalink(); ?>" class="entry-link icon-link">
+            <span class="icon meditech-chevron-left purpleArrowIcon"></span>
+            <span class="text">
 					<?php _e( 'קראו עוד', 'ystheme' ); ?>
 				</span>
-			</a>
-		</footer>
+        </a>
+    </footer>
 </div>
